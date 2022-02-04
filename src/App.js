10ff1ddce9e8da9761df861cfa120f14056e9f46
LeanC100 <lcoca@mailexternos.com.ar>
2022-02-04 10:56:02 -0300

    creacion de un componente react parte 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React, {Component} from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponents'
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div >
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/"> Reistonante con fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render(){
+    return (
+      <div >
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/"> Reistonante con fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} className="d-flex justify-content-center"/>
+      </div>
+    );
+  }
 }
 
 export default App;
