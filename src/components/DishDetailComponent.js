0c286bad2a9650fc64c53864ef05228bc34a41ab
LeanC100 <lcoca@mailexternos.com.ar>
2022-02-20 22:37:15 -0300

    functional components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody,
   CardTitle, } from 'reactstrap'; 
 
-class DishDetail extends Component {
-
-        menu(){
-          const comment =this.props.dish.comments.map((comments) => {
-            var d = new Date(comments.date); 
+        function RenderComments({comments}){
+          const comment =  comments.map((comment) => {
+            console.log(comment)
+            var d = new Date(comment.date); 
             let date = (d.toDateString() /* +" " + d.toTimeString().split(/\s/)[0] */);
             /* {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} */
             return (
-                <div key={comments.id} className="comments"> 
-                  <span>{date}</span>
-                  <h4>{comments.comment}</h4>
-                  <p>{comments.author}</p>
-                </div>
+                  <div key={comment.id} className="comments"> 
+                    <span>{date}</span>
+                    <h4>{comment.comment}</h4>
+                    <p>{comment.author}</p>
+                  </div>
             );
-          });
+          })
           return comment
-        }
+        };
       
-      render() {
-        const dish_detail = this.props.dish
-        console.log(this.props.dish)
+        const DishDetail = (props) => {
+          const dish_detail = props.dish
+          console.log(props.dish)
           if (dish_detail != null)
 
             return(
-              <div>
-                <h2> Dish Detail </h2>
-
-                <div className='row'>
+              <div className="container">
+              <div className='row'>
+                <div className='col-12'>
+                  <h2> Dish Detail </h2>
+                </div>
                   <div  className="col-12 col-md-5 m-1">
 
                   <Card key={dish_detail.id}  onClick={() => this.props.onClick(dish_detail.id)}>
@@ -41,16 +41,16 @@ class DishDetail extends Component {
                   </Card>
                   </div>
                   <div  className="col-12 col-md-5 m-1">
-                      <div>{this.menu()}</div>
+                      <RenderComments comments={props.dish.comments} />
+                      <h1>hola</h1>
                   </div>
-                </div>
+              </div>
               </div>
             );
           else
               return(
                   <div></div>
               );
-      }
 
 }
 
